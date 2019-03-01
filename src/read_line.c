930a95ca26c184aec0f9718e2066a09293acdf0d
arwn <windham.aren@protonmail.com>
2019-03-01 14:14:03 -0800

    exit on ^D

--- src/read_line.c
+++ src/read_line.c
@@ -3,21 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   read_line.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
+/*   By: awindham <awindham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 13:13:30 by awindham          #+#    #+#             */
-/*   Updated: 2019/03/01 13:29:40 by zfaria           ###   ########.fr       */
+/*   Updated: 2019/03/01 14:13:38 by awindham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <minishell.h>
+#include <stdlib.h>
 
 char	*read_line()
 {
 	char *done;
 
 	prompt();
-	get_next_line(0, &done, 512);
+	if (get_next_line(0, &done, 512) == 0)
+		exit(-1);
 	return (done);
 }
