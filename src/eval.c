3dbfb0fea8223fd89305e1e2044a872705862f3a
zacheryfaria <zacheryfaria@gmail.com>
2019-03-04 10:37:26 -0800

    removed debugging stuff

--- src/eval.c
+++ src/eval.c
@@ -6,7 +6,7 @@
 /*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 13:12:47 by awindham          #+#    #+#             */
-/*   Updated: 2019/03/04 10:30:30 by zfaria           ###   ########.fr       */
+/*   Updated: 2019/03/04 10:37:20 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,10 @@ char	*eval(char **tokens)
 	char *func;
 	int	status;
 
-	for (int i = 0; i < 2; i++)
-	{
-		ft_printf("%s\n", tokens[i]);
-	}
-
 	func = ft_strnew(ft_strlen(g_path[3]) + ft_strlen(tokens[0]) + 2);
 	func = ft_strcat(func, g_path[3]);
 	func = ft_strcat(func, "/");
 	func = ft_strcat(func, tokens[0]);
-	
 	if (fork() == 0)
 		execve(func, tokens, environ);
 	else
