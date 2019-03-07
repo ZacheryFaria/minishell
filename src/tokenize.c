b20931bb25e79e7e2b4aa22bc0412de33ecba4c5
zacheryfaria <zacheryfaria@gmail.com>
2019-03-07 13:03:41 -0800

    Fixed tabs

--- src/tokenize.c
+++ src/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: zfaria <zfaria@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 13:14:25 by awindham          #+#    #+#             */
-/*   Updated: 2019/03/07 12:49:00 by zfaria           ###   ########.fr       */
+/*   Updated: 2019/03/07 12:59:09 by zfaria           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	**tokenize(char *buf)
 	char	*bak;
 	int		i;
 
-	done = ft_strsplit(buf, ' ');
+	done = ft_strsplitws(buf);
 	i = 0;
 	while (done[i])
 	{
@@ -32,13 +32,7 @@ char	**tokenize(char *buf)
 			done[i] = ft_strjoin(g_home, bak + 1);
 			free(bak);
 		}
-		bak = ft_strtrim(done[i]);
-		free(done[i]);
-		done[i] = bak;
-		if (done[i][0] == 0)
-			ft_strdel(&done[i]);
-		else
-			i++;
+		i++;
 	}
 	return (done);
 }
